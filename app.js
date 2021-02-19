042bc5e52622e4f397f2220e0f4981786f9f52f9
Richard Rosario <catrosits@gmail.com>
2021-02-19 12:36:34 +0800

    creating routes

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
-const app = express()
 require('dotenv').config()
+const userRoutes = require('./routes/user')
 
-app.get('/', (req, res) => {
-    res.send('This is node..')
-})
+// db
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+}).then(() => console.log('We are connected to DB'))
+
+// app
+const app = express()
+
+// routes
+
+app.use(userRoutes)
 
 const port = process.env.PORT || 8080
 
